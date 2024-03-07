2a66dcb524a249455174a9478d8c5ead36b75c54
kuui <kawase0987@gmail.com>
2024-03-07 10:39:06 +0900

    新規作成

---
+++ src/main/java/com/example/demo/ExpandingCircleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpandingCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpandingCircleApplication.class, args);
+	}
+
+}
