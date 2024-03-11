31eb34a10b85c42cfbd7b43dca62b0f456ad1aea
kuuiryoga <kawase0987@gmail.com>
2024-03-11 09:08:47 +0900

    [テストデータ]テスト画面　削除　2024/03/11 川瀬

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.model.Forum;
 import com.example.demo.model.Users;
 import com.example.demo.service.UsersService;
 
